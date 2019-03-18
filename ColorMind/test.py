13bc23b3da78f5bcf4a0d12d996bc851a5f59dd1
Ojas B N <ojasbn@er-10-131-29-174.wireless.umn.edu>
2019-03-18 17:10:37 -0500

    changes

--- ColorMind/test.py
+++ ColorMind/test.py
@@ -1,23 +1,27 @@
 from PIL import Image
+import requests
+from requests.auth import HTTPBasicAuth
+
+
 image = Image.open('P1.jpg','r')
-pix_val = list(image.getdata()) #list of tuples
+pix_val = list(image.getdata()) 	#list of tuples
 
 pix_val_2 = [list(elem) for elem in pix_val]
-#print(pix_val)
 
-#print(type(pix_val_2[0]))
 
 #curl 'http://colormind.io/api/' --data-binary '{"input":pix_val_2,"model":"default"}'
 
-import requests
-from requests.auth import HTTPBasicAuth
-payload = {'input':pix_val_2}
-response = requests.get('http://colormind.io/api/',params=payload)
+
+payload = '{"input":[[44,43,44],[90,83,82],"N","N","N"],"model":"default"}'
+arr='[[44,43,44],[90,83,82],"N","N","N"]'
+
+payload2 = '{"input":' + arr + ',"model":"default"}'
+response = requests.post('http://colormind.io/api/',payload2.encode())
 
 foo = response.json()
 
 print(type(foo))
 
-print(size(foo))
+#print(size(foo))
 
 print(foo)
