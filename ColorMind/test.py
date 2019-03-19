011489547025dfe3ea6df602d4e4bf787d3d3c5f
Anick Saha <anicksaha05@gmail.com>
2019-03-18 20:22:42 -0500

    Changes to Test.py in ColorMind.

--- ColorMind/test.py
+++ ColorMind/test.py
@@ -4,26 +4,29 @@ from requests.auth import HTTPBasicAuth
 
 
 image = Image.open('P1.jpg','r')
-pix_val = list(image.getdata()) 	#list of tuples
+#print(image.size)
+image = image.resize((100,100),Image.ANTIALIAS)
+#print(image.size)
+#image.save("P1_scaled.jpg",quality=95)
 
+pix_val = list(image.getdata()) 	#list of tuples
 pix_val_2 = [list(elem) for elem in pix_val]
 
 
 #curl 'http://colormind.io/api/' --data-binary '{"input":pix_val_2,"model":"default"}'
-
-payload1 = '{"input":[[44,43,44],[90,83,82],"N","N","N"],"model":"default"}'
-
-s0 = '[[44,43,44],[90,83,82],[90,83,81]]'
-payload2 = '{"input":' + s0 + ',"model":"default"}'
+#payload1 = '{"input":[[44,43,44],[90,83,82],"N","N","N"],"model":"default"}'
+#s0 = '[[44,43,44],[90,83,82],[90,83,81]]'
 
 s = str(pix_val_2)
-#print(type(s))
+#print(s)
 
 payload = '{"input":' + s + ',"model":"default"}'
 print(type(payload))
 
-response = requests.post('http://colormind.io/api/',payload2.encode())
+response = requests.post('http://colormind.io/api/',payload.encode())
+
 responseJson = response.json()
+print(responseJson)
 #print(type(responseJson))
 result = responseJson.get('result');
 print(result)
