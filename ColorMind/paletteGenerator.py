36aa9976e997a66f2856acdea048dc575e309f0c
Ojas B N <ojasbn@Ojass-MacBook-Air.local>
2019-04-09 19:18:44 -0500

    Sequential reading of images to generate palette array

--- ColorMind/paletteGenerator.py
+++ ColorMind/paletteGenerator.py
@@ -6,55 +6,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
 import glob
+import json
 
 f_name='P1'
 
 
 #Load Image
 
-
-image = Image.open(open('utils/'+f_name+'.jpg','rb'))
-#print(image.size
-image = image.resize((255,255),Image.ANTIALIAS)
-#print(image.size)
-#image.save("P2_scaled.jpg",quality=95)
-
-
-#Generate pixel
-pix_val = list(image.getdata()) 	#list of tuples
-pix_val_2 = [list(elem) for elem in pix_val]
-
-
-#curl 'http://colormind.io/api/' --data-binary '{"input":pix_val_2,"model":"default"}'
-#payload1 = '{"input":[[44,43,44],[90,83,82],"N","N","N"],"model":"default"}'
-#s0 = '[[44,43,44],[90,83,82],[90,83,81]]'
-
-#Prepare payload
-s = str(pix_val_2)
-payload = '{"input":' + s + ',"model":"default"}'
-print(type(payload))
-
-
-#Call API
-response = requests.post('http://colormind.io/api/',payload.encode())
-
-#Parse Result
-responseJson = response.json()
-print(responseJson)
-
-result = responseJson.get('result');
-print(result)
+out ={}
+images = glob.glob('utils/*.jpg')
+for image in images:
+	with open(image,'rb') as file:
+		file_name=image.split('/')[1].split('.')[0]
+		#print file_name
+		img=Image.open(file)
+		
+		img = img.resize((255,255),Image.ANTIALIAS)
+		
+		pix_val = list(img.getdata()) 	#list of tuples
+		pix_val_2 = [list(elem) for elem in pix_val]
+		#Prepare payload
+		s = str(pix_val_2)
+		payload = '{"input":' + s + ',"model":"default"}'
+		try:
+			print "Generating palette for "+file_name
+			response = requests.post('http://colormind.io/api/',payload.encode())
+			responseJson = response.json()
+			result = responseJson.get('result');
+			out[file_name]=result
+		except:
+			continue
 
 
+print json.dumps(out)
 
-#Displaying the palette
-# palette = np.array(result, dtype=np.uint8)
-# ind = [[0,1,2,3,4]]
-# ind=np.array(ind)
-# plt.imshow(palette[ind])
-
-#Saving the palette
-#io.imsave(''+f_name+'_palette.jpg',palette[ind])
-
-
-#	plt.show()
