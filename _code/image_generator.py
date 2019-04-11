3c5aae7e6d6877748cd2568f920fd77877516eb0
Ojas B N <ojasbn@Ojass-MacBook-Air.local>
2019-04-11 02:05:26 -0500

    read and write into folder structure

--- _code/image_generator.py
+++ _code/image_generator.py
@@ -6,21 +6,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io
 import glob
+from argparse import *
 
 
-#f_name='P1'
 
-out ={}
 
-images = glob.glob('data/*.jpg')
-#Load Image
-for image in images:
-	with open(image,'rb') as file:
-		file_name=image.split('/')[1].split('.')[0]
-		img=Image.open(file)
-		img=img.resize((255,255),Image.ANTIALIAS)
-		img.save('output/'+file_name+'.jpg')
-		pix_val = list(img.getdata())
-		pix_val_2 = [list(elem) for elem in pix_val]
-		out[file_name]=pix_val_2
-		
+
+def main():
+
+	input_parser = ArgumentParser()
+	input_parser.add_argument('-i', '-dir', required=True, nargs=1, metavar=('f1'))
+	parser=input_parser.parse_args()
+	inputdir=parser.i
+	
+	out ={}
+
+
+	images = glob.glob('data/raw_images/'+inputdir[0]+'/*.jpg')
+	path = os.getcwd()
+
+	outputpath= path+'/data/reduced_images/'+inputdir[0]
+	os.mkdir(outputpath)
+	
+	
+
+	for image in images:
+		with open(image,'rb') as file:
+			file_name=image.split('/')[3].split('.')[0]
+			img=Image.open(file)
+			img=img.resize((255,255),Image.ANTIALIAS)
+			img.save(outputpath+'/'+file_name+'.jpg','JPEG')
+			
+			pix_val = list(img.getdata())
+			pix_val_2 = [list(elem) for elem in pix_val]
+			out[file_name]=pix_val_2
+	#print out
+
+
+if __name__ == '__main__':
+    main()		
--- _code/palette_generator.py
+++ _code/palette_generator.py
@@ -7,34 +7,49 @@ import matplotlib.pyplot as plt
 from skimage import io
 import glob
 import json
+from argparse import *
 
 
 #Load Image
 
-out ={}
-images = glob.glob('utils/P701-805/*.jpg')
-for image in images:
-	with open(image,'rb') as file:
-		file_name=image.split('/')[2].split('.')[0]
+def main():
 
-		img=Image.open(file)
-		img = img.resize((255,255),Image.ANTIALIAS)
+	input_parser = ArgumentParser()
+	input_parser.add_argument('-i', '-dir', required=True, nargs=1, metavar=('f1'))
+	parser=input_parser.parse_args()
+	inputdir=parser.i
+
+	out ={}
+	images = glob.glob('data/reduced_images/'+inputdir[0]+'/*.jpg')
+	for image in images:
+		with open(image,'rb') as file:
+			file_name=image.split('/')[3].split('.')[0]
+
+			img=Image.open(file)
+			#img = img.resize((255,255),Image.ANTIALIAS)
 		
-		pix_val = list(img.getdata()) 	
-		pix_val_2 = [list(elem) for elem in pix_val]
+			pix_val = list(img.getdata()) 	
+			pix_val_2 = [list(elem) for elem in pix_val]
 		
-		#Prepare payload
-		s = str(pix_val_2)
-		payload = '{"input":' + s + ',"model":"default"}'
-		try:
+			#Prepare payload
+			s = str(pix_val_2)
+			payload = '{"input":' + s + ',"model":"default"}'
+			try:
 			#print "Generating palette for "+file_name
-			response = requests.post('http://colormind.io/api/',payload.encode())
-			responseJson = response.json()
-			result = responseJson.get('result');
-			out[file_name]=result
-		except:
-			continue
+				response = requests.post('http://colormind.io/api/',payload.encode())
+				responseJson = response.json()
+				result = responseJson.get('result');
+				out[file_name]=result
+			except:
+				continue
 
 
-print json.dumps(out)
+	with open('data/palette_data/'+inputdir[0]+'.json','w') as f:
+		json.dump(out,f)
+
+
+
+
+if __name__ == '__main__':
+    main()	
 
