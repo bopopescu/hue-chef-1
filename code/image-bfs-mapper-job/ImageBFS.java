153732d49bdb0bed5bef9cb48862d0d82b2e0e0f
Anick Saha <anicksaha05@gmail.com>
2019-04-16 20:14:19 -0500

    Indentation.

--- code/image-bfs-mapper-job/ImageBFS.java
+++ code/image-bfs-mapper-job/ImageBFS.java
@@ -7,22 +7,22 @@ import java.io.IOException;
 public class ImageBFS {
 
 	public static void main(String[] args) throws IOException {
-		BufferedImage bi = ImageIO.read(new File("./P1.jpg"));
-
+        BufferedImage bi = ImageIO.read(new File("./P1.jpg"));
+        
         int h = bi.getHeight();
         int w = bi.getWidth();
 
         System.out.println(h + " " + w);
 
-		for (int x = 0; x < w; x++) {
-    		for (int y = 0; y < h; y++) {
-        		int pixel = bi.getRGB(x, y);
-        		int red = (pixel >> 16) & 0xff;
-        		int green = (pixel >> 8) & 0xff;
-        		int blue = (pixel) & 0xff;
-        		// System.out.println("red: " + red + ", green: " + green + ", blue: " + blue);                                                                                                                                                  
-    		}
-		}
-	}
+        for (int x = 0; x < w; x++) {
+            for (int y = 0; y < h; y++) {
+                int pixel = bi.getRGB(x, y);
+                int red = (pixel >> 16) & 0xff;
+                int green = (pixel >> 8) & 0xff;
+                int blue = (pixel) & 0xff;
+                // System.out.println("red: " + red + ", green: " + green + ", blue: " + blue);
+            }
+        }
+    }
 }
 
