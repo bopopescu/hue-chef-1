badf0d9a825aa940513d774c1006a0d8dc393df0
Anick Saha <anicksaha05@gmail.com>
2019-04-16 20:08:53 -0500

    Image BFS done.

--- code/image-bfs-mapper-job/ImageBFS.java
+++ code/image-bfs-mapper-job/ImageBFS.java
@@ -1,18 +1,26 @@
 import java.util.*;
-
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ImageBFS {
 
-	public static void main() {
-		BufferedImage bi = ImageIO.read(new File("C:\\images\\Sunset.jpg"));
+	public static void main(String[] args) throws IOException {
+		BufferedImage bi = ImageIO.read(new File("./P1.jpg"));
 
-		for (int x = 0; x < bi.getWidth(); x++) {
-    		for (int y = 0; y < bi.getHeight(); y++) {
+        int h = bi.getHeight();
+        int w = bi.getWidth();
+
+        System.out.println(h + " " + w);
+
+		for (int x = 0; x < w; x++) {
+    		for (int y = 0; y < h; y++) {
         		int pixel = bi.getRGB(x, y);
         		int red = (pixel >> 16) & 0xff;
         		int green = (pixel >> 8) & 0xff;
         		int blue = (pixel) & 0xff;
-        		System.out.println("red: " + red + ", green: " + green + ", blue: " + blue);                                                                                                                                                  
+        		// System.out.println("red: " + red + ", green: " + green + ", blue: " + blue);                                                                                                                                                  
     		}
 		}
 	}
