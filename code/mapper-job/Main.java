c1613387b3ee17bef54752d31dd98634cb10770c
Anick Saha <anicksaha05@gmail.com>
2019-04-16 21:25:56 -0500

    Folders and files.

--- code/mapper-job/Main.java
+++ code/mapper-job/Main.java
@@ -4,9 +4,23 @@ import java.io.IOException;
 
 // Driver Program
 public class Main {
-
 	public static void main(String[] args) throws IOException {
-        String filePath = "./P1.jpg";
-        Utils.ImageBFS(filePath);
+        //System.out.println("OK!");
+        File[] files = new File("./data/").listFiles();
+        operateOnFiles(files);
+        //String filePath = "./P1.jpg";
+        //Utils.ImageBFS(filePath);
     }
+
+    // Recursive
+    public static void operateOnFiles(File[] files) {
+    	for (File file : files) {
+        	if (file.isDirectory()) {
+            	System.out.println("Directory: " + file.getName());
+            	operateOnFiles(file.listFiles()); 
+        	} else {
+            	//System.out.println("File: " + file.getName());
+        	}
+    	}
+	}
 }
