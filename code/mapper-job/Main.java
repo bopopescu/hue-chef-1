e7a076637a0e1c4e8ee76343a456ee0240656694
Anick Saha <anicksaha05@gmail.com>
2019-04-16 21:32:59 -0500

    Indentation.

--- code/mapper-job/Main.java
+++ code/mapper-job/Main.java
@@ -5,22 +5,22 @@ import java.io.IOException;
 // Driver Program
 public class Main {
 	public static void main(String[] args) throws IOException {
-        //System.out.println("OK!");
-        File[] files = new File("./data/").listFiles();
-        operateOnFiles(files);
-        //String filePath = "./P1.jpg";
-        //Utils.ImageBFS(filePath);
+		//System.out.println("OK!");
+		File[] files = new File("./data/").listFiles();
+		operateOnFiles(files);
+		//String filePath = "./P1.jpg";
+		//Utils.ImageBFS(filePath);
     }
 
-    // Recursive
-    public static void operateOnFiles(File[] files) {
-    	for (File file : files) {
-    		if (file.isDirectory()) {
-    			System.out.println("Directory: " + file.getName());
-    			operateOnFiles(file.listFiles()); 
-        	} else {
-        		//System.out.println("File: " + file.getName());
-        	}
-    	}
+	// Recursive
+	public static void operateOnFiles(File[] files) {
+		for (File file : files) {
+			if (file.isDirectory()) {
+				System.out.println("Directory: " + file.getName());
+				operateOnFiles(file.listFiles()); 
+			} else {
+				//System.out.println("File: " + file.getName());
+			}
+		}
 	}
 }
