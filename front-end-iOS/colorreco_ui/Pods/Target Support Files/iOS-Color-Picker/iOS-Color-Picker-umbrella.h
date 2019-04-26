da767fcdbbfa12fcc2d655caa03fab9fc72d3ba9
ojasbn <ojasbond@gmail.com>
2019-04-26 10:16:21 -0500

    color picker flow

---
+++ front-end-iOS/colorreco_ui/Pods/Target Support Files/iOS-Color-Picker/iOS-Color-Picker-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FCColorPickerViewController.h"
+
+FOUNDATION_EXPORT double iOS_Color_PickerVersionNumber;
+FOUNDATION_EXPORT const unsigned char iOS_Color_PickerVersionString[];
+
--- front-end-iOS/colorreco_ui/colorreco_ui/ColorViewController.swift
+++ front-end-iOS/colorreco_ui/colorreco_ui/ColorViewController.swift
@@ -7,18 +7,65 @@
 //
 
 import UIKit
+import iOS_Color_Picker
 
-class ColorViewController: UIViewController {
+class ColorViewController:
+UIViewController,FCColorPickerViewControllerDelegate {
+    var color = UIColor(red: 1, green: 165/255, blue: 0, alpha: 1)
+    var ans = UIColor(red: 1, green: 165/255, blue: 0, alpha: 1)
 
+    
+    func colorPickerViewController(_ colorPicker: FCColorPickerViewController, didSelect color: UIColor) {
+        self.ans = color
+        displayView.backgroundColor = color
+        dismiss(animated: true)
+        print("ans")
+        print(ans.rgb()!)
+        
+    }
+    
+    func colorPickerViewControllerDidCancel(_ colorPicker: FCColorPickerViewController) {
+        
+        dismiss(animated: true)
+        
+    }
+    
+    
+    
+    
+    
+    @IBOutlet weak var displayView: UIView!
+    
+    
+
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+       
+       
+        
 
         // Do any additional setup after loading the view.
     }
+ 
     
     @IBAction func backButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func chooseColorPressed(_ sender: Any) {
+        print("call!!")
+        let colorPicker = FCColorPickerViewController.colorPicker()
+        colorPicker.color = self.color
+        colorPicker.delegate = self
+        colorPicker.modalPresentationStyle = .formSheet
+        present(colorPicker, animated: true)
+        
+    }
+    
     
     
     /*
@@ -32,3 +79,26 @@ class ColorViewController: UIViewController {
     */
 
 }
+extension UIColor {
+    
+    func rgb() -> Int? {
+        var fRed : CGFloat = 0
+        var fGreen : CGFloat = 0
+        var fBlue : CGFloat = 0
+        var fAlpha: CGFloat = 0
+        if self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha) {
+            let iRed = Int(fRed * 255.0)
+            let iGreen = Int(fGreen * 255.0)
+            let iBlue = Int(fBlue * 255.0)
+            let iAlpha = Int(fAlpha * 255.0)
+            
+            //  (Bits 24-31 are alpha, 16-23 are red, 8-15 are green, 0-7 are blue).
+            let rgb = (iAlpha << 24) + (iRed << 16) + (iGreen << 8) + iBlue
+            return rgb
+        } else {
+            // Could not extract RGBA components:
+            return nil
+        }
+    }
+}
+
