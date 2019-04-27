af725e0aa02ecb6d90ecd4fcb850a4b311b801b7
ojasbn <ojasbond@gmail.com>
2019-04-27 18:23:38 -0500

    stub API created

--- front-end-iOS/colorreco_ui/Pods/Target Support Files/Pods-colorreco_ui/Pods-colorreco_ui-frameworks.sh
+++ front-end-iOS/colorreco_ui/Pods/Target Support Files/Pods-colorreco_ui/Pods-colorreco_ui-frameworks.sh
@@ -153,9 +153,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/iOS-Color-Picker/iOS_Color_Picker.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyJSON/SwiftyJSON.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/iOS-Color-Picker/iOS_Color_Picker.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- front-end-iOS/colorreco_ui/colorreco_ui/DisplayViewController.swift
+++ front-end-iOS/colorreco_ui/colorreco_ui/DisplayViewController.swift
@@ -7,21 +7,38 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class DisplayViewController: UIViewController {
     var ipcolor = UIColor(red: 1, green: 165/255, blue: 0, alpha: 1)
-
+    var opcolor = UIColor(red: 1, green: 165/255, blue: 0, alpha: 1)
+    
+    let APP_URL = "https://jsonplaceholder.typicode.com/todos/1"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         chosenColorView.backgroundColor = ipcolor
+        //outputColorView1.backgroundColor = opcolor
+       // outputColorView2.backgroundColor = opcolor
+       // outputColorView3.backgroundColor = opcolor
         
-
+        getColorData(url: APP_URL,parameters: ["":""])
+        
+        
+        
         // Do any additional setup after loading the view.
     }
-   
+    
     
     
     @IBOutlet weak var chosenColorView: UIView!
+    @IBOutlet weak var outputColorView1: UIView!
+    
+    @IBOutlet weak var outputColorView2: UIView!
+    
+    @IBOutlet weak var outputColorView3: UIView!
     
     @IBAction func backButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -30,14 +47,30 @@ class DisplayViewController: UIViewController {
     @IBAction func homeButtonPressed(_ sender: Any) {
         performSegue(withIdentifier: "unwindSegueToHome", sender: self)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func getColorData(url:String , parameters:[String:String]){
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess{
+                let colorJSON : JSON = JSON(response.result.value!)
+                print(colorJSON)
+                //Handle output
+                
+            }
+            else{
+                print("Error:\(String(describing: response.result.error))")
+            }
+        }
+        
     }
-    */
-
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
