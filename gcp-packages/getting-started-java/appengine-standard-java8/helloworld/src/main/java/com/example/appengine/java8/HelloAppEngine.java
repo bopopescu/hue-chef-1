775e514d87b95ed8d9cd27084ac6ab4f065cf5d7
Anick Saha <anicksaha05@gmail.com>
2019-05-02 17:39:37 -0500

    Walter random hours.

--- gcp-packages/getting-started-java/appengine-standard-java8/helloworld/src/main/java/com/example/appengine/java8/HelloAppEngine.java
+++ gcp-packages/getting-started-java/appengine-standard-java8/helloworld/src/main/java/com/example/appengine/java8/HelloAppEngine.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.appengine.java8;
 
 // [START example]
@@ -35,10 +19,12 @@ public class HelloAppEngine extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     Properties properties = System.getProperties();
 
-    response.setContentType("text/plain");
-    
-    //PrintWriter out = response.getWriter();
-    response.getWriter().println(request.getParameter("x") + "Anick");
+    String pixel = request.getParameter("color");
+
+    String result = ServerUtils.GetReco(pixel);
+
+    response.setContentType("text/plain"); 
+    response.getWriter().println(pixel + result);
 
     //response.getWriter().println("Hello App Engine - Standard using "
       //      + SystemProperty.version.get() + " Java "
---
+++ gcp-packages/getting-started-java/appengine-standard-java8/helloworld/src/main/java/com/example/appengine/java8/ServerUtils.java
@@ -0,0 +1,32 @@
+package com.example.appengine.java8;
+
+public class ServerUtils {
+
+  static {
+    // static block to load map 
+  }
+
+
+  public static String GetReco(String pixel) {
+
+    //String[] rgb = pixel.split("|"); 
+  
+
+    //int red = Integer.parseInt(rgb[0]);
+    //int green = Integer.parseInt(rgb[1]);
+    //int blue = Integer.parseInt(rgb[2]);
+    
+    // do job
+
+
+    // stubbing for now.
+    
+    String color1 = "255|128|20";
+    String color2 = "255|128|20";
+    String color3 = "255|128|20";
+
+    String ans = color1 + "#" + color2 + "#" + color3;
+    System.out.println(ans);
+    return ans;
+  }
+}
