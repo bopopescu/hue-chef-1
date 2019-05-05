2db6986cd4c9f31b4adf20393d409f8f9ea06775
Anick Saha <anicksaha05@gmail.com>
2019-05-04 20:28:10 -0500

    z

--- code/mapper-job/Main.java
+++ code/mapper-job/Main.java
@@ -34,7 +34,7 @@ public class Main {
 		loadPaletteData(paletteFiles);
 
 		// Operate on all the files.
-		File[] files = new File("./data/").listFiles();
+		File[] files = new File("./data-test/").listFiles();
 		count = 0;
 		edgeMap = new HashMap<>();
 		operateOnFiles(files);
