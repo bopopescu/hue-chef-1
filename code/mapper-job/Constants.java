6404d190dc3410ef2acb392c53d39fc21fc9b81b
Anick Saha <anicksaha05@gmail.com>
2019-05-05 22:30:28 -0500

    A1

--- code/mapper-job/Constants.java
+++ code/mapper-job/Constants.java
@@ -3,7 +3,7 @@
 
 public class Constants {
     
-    public static int DELTA = 25; // RGB +/- relaxation factor.
+    public static int DELTA = 30; // RGB +/- relaxation factor.
     public static int MIN_THRESHOLD = 40; // The threshold after which we can say that two color nodes share a boundary.
 
     public static String DELIM = "|";
